c58f6be340571ca74851065e04ada95a00d79cfb
anirudhgurjar <anirudh.gurjar@gmail.com>
2019-01-03 23:07:20 -0500

    .

--- nowui/vue/server.js
+++ nowui/vue/server.js
@@ -3,5 +3,9 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
+app.get(/.*/,function(req,res){
+    res.sendfile(__dirname + "/dist/index.html");
+});
+
 app.listen(port);
